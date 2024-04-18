8bb83f0c94a3c4900ad83102369d4b111c3cca19
karelkruijer <142407338+karelkruijer@users.noreply.github.com>
2024-04-18 13:00:36 +0200

    Add files via upload

---
+++ js/navbar.js
@@ -0,0 +1,4 @@
+document.querySelector('.hamburger-menu').addEventListener('click', function() {
+    document.querySelector('.nav-links').classList.toggle('active');
+    document.querySelector('.hamburger-menu').classList.toggle('active');
+});
