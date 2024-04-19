62ebe8be780b6194eb79d7603300dab2e4575610
karelkruijer <142407338+karelkruijer@users.noreply.github.com>
2024-04-19 15:40:19 +0200

    Add files via upload

--- js/navbar.js
+++ js/navbar.js
@@ -2,3 +2,25 @@ document.querySelector('.hamburger-menu').addEventListener('click', function() {
     document.querySelector('.nav-links').classList.toggle('active');
     document.querySelector('.hamburger-menu').classList.toggle('active');
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const images = [
+        'url("img/iStock-1328317040.jpg")',
+        'url("img/iStock-1298207802.jpg")',
+        'url("img/iStock-1407976114.jpg")',
+        'url("img/iStock-1481069966.jpg")'
+    ];
+
+    let currentIndex = 0;
+    const backgroundElement = document.getElementById('background-slider');
+
+    // Functie om de achtergrondafbeelding te wijzigen
+    function changeBackground() {
+        backgroundElement.style.backgroundImage = images[currentIndex];
+        currentIndex = (currentIndex + 1) % images.length; // Verplaats naar de volgende afbeelding
+    }
+
+    // Stel de eerste afbeelding in en start de interval
+    changeBackground();
+    setInterval(changeBackground, 10000); // Wissel elke 10 seconden
+});
